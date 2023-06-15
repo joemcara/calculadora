7f2329000c2d5dcc529310dbb453150b527172d4
Joseph carabajo <joemcara@espol.edu.ec>
2023-06-14 22:00:14 -0800

    version 1.0.2

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -2,10 +2,13 @@ let funcionPredeterminada = () => {
     const formulario = document.getElementById('formulario')
     formulario.addEventListener('submit',(evento) => {
         evento.preventDefault();
-        let valorHora = document.getElementById('valorPorHora').value 
-        let desing = document.getElementById('interfazHoras').value
+        let valorHora = parseFloat(document.getElementById('valorPorHora').value)
+        let desing = parseFloat(document.getElementById('interfazHoras').value)
+        
         let resultado = valorHora + desing;
+
         document.getElementById('valorTotal').value = resultado;
+
     })
 }
 
